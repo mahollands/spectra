d95a920533a39b48da9814b8fa477b0eb658bdff
mahollands <M.Hollands.1@warwick.ac.uk>
2019-02-26 17:06:15 +0000

    imports all from misc to fix black_body

--- spectra/spec_class.py
+++ spectra/spec_class.py
@@ -9,7 +9,7 @@ import astropy.constants as const
 from scipy.interpolate import interp1d, Akima1DInterpolator as Ak_i
 from .synphot import mag_calc_AB
 from .reddening import A_curve
-from .misc import vac_to_air, air_to_vac, convolve_gaussian, convolve_gaussian_R
+from .misc import *
 
 class Spectrum(object): 
   """
