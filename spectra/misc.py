519c8aa6524a171e5d72bc679fd6b2e430355382
mahollands <M.Hollands.1@warwick.ac.uk>
2019-02-27 11:20:44 +0000

    Added __all__

--- spectra/misc.py
+++ spectra/misc.py
@@ -1,9 +1,23 @@
-import numpy as np
+import numpy as _np
 from scipy.interpolate import interp1d
 from scipy.optimize import leastsq
 from scipy.special import wofz
 from functools import reduce
 import operator
+
+__all__ = [
+  "jangstrom",
+  "voigt",
+  "vac_to_air",
+  "air_to_vac",
+  "convolve_gaussian",
+  "convolve_gaussian_R",
+  "black_body",
+  "sky_line_fwhm",
+  "keep_points",
+  "Lanczos",
+]
+
 
 jangstrom = \
   "$\mathrm{erg}\;\mathrm{s}^{-1}\,\mathrm{cm}^{-2}\,\mathrm{\AA}^{-1}$"
@@ -12,9 +26,9 @@ def voigt(x, x0, fwhm_g, fwhm_l):
   sigma = voigt.Va*fwhm_g
   z = ((x-x0) + 0.5j*fwhm_l)/(sigma*voigt.Vb)
   return wofz(z).real/(sigma*voigt.Vc)
-voigt.Va = 1/(2*np.sqrt(2*np.log(2)))
-voigt.Vb = np.sqrt(2)
-voigt.Vc = np.sqrt(2*np.pi)
+voigt.Va = 1/(2*_np.sqrt(2*_np.log(2)))
+voigt.Vb = _np.sqrt(2)
+voigt.Vc = _np.sqrt(2*_np.pi)
 
 def vac_to_air(Wvac):
   """
@@ -56,16 +70,16 @@ def convolve_gaussian(x, y, FWHM):
     return N_out
 
   #oversample data by at least factor 10 (up to 20).
-  xi = np.linspace(x[0], x[-1], next_pow_2(10*len(x)))
+  xi = _np.linspace(x[0], x[-1], next_pow_2(10*len(x)))
   yi = interp1d(x, y)(xi)
 
-  yg = np.exp(-0.5*((xi-x[0])/sigma)**2) #half gaussian
+  yg = _np.exp(-0.5*((xi-x[0])/sigma)**2) #half gaussian
   yg += yg[::-1]
-  yg /= np.sum(yg) #Norm kernel
+  yg /= _np.sum(yg) #Norm kernel
 
-  yiF = np.fft.fft(yi)
-  ygF = np.fft.fft(yg)
-  yic = np.fft.ifft(yiF * ygF).real
+  yiF = _np.fft.fft(yi)
+  ygF = _np.fft.fft(yg)
+  yic = _np.fft.ifft(yiF * ygF).real
 
   return interp1d(xi, yic)(x)
 #
@@ -76,7 +90,7 @@ def convolve_gaussian_R(x, y, R):
   rather than a specfied FWHM. Essentially this amounts to convolving
   along a log-uniform x-axis instead.
   """
-  return convolve_gaussian(np.log(x), y, 1./R)
+  return convolve_gaussian(_np.log(x), y, 1./R)
 #
 
 def black_body(x, T, norm=True):
@@ -85,17 +99,17 @@ def black_body(x, T, norm=True):
   T in Kelvin
   returns un-normed spectrum
   """
-  logf = np.empty_like(x,dtype='float')
+  logf = _np.empty_like(x,dtype='float')
   Q = 143877516. /(x*T) # const. = ( h * c )/( 1e-10 * kB )
   lo = Q < 10.
   hi = ~lo
   #log form needed to stop overflow in x**-5
   #for Q>7. exp(Q)==expm1(Q) to better than 0.1%.
-  logf[lo] = -5. * np.log( x[lo] ) - np.log( np.expm1(Q[lo]) )
-  logf[hi] = -5. * np.log( x[hi] ) - Q[hi]
+  logf[lo] = -5. * _np.log( x[lo] ) - _np.log( _np.expm1(Q[lo]) )
+  logf[hi] = -5. * _np.log( x[hi] ) - Q[hi]
   if norm:
     logf -= logf.max() #normalise to peak at 1.
-  return np.exp( logf )
+  return _np.exp( logf )
 #
 
 def sky_line_fwhm(S, x0, dx=5.):
@@ -106,13 +120,13 @@ def sky_line_fwhm(S, x0, dx=5.):
   def sky_residual(params, S):
     A, x0, fwhm, C = params
     xw = fwhm /2.355
-    y_fit = A*np.exp(-0.5*((S.x-x0)/xw)**2) + C
+    y_fit = A*_np.exp(-0.5*((S.x-x0)/xw)**2) + C
     return (S.y - y_fit)/S.e
 
   Sc = S.clip(x0-dx, x0+dx)
-  guess = Sc.y.max(), x0, 2*np.diff(Sc.x).mean(), Sc.y.min()
+  guess = Sc.y.max(), x0, 2*_np.diff(Sc.x).mean(), Sc.y.min()
   res = leastsq(sky_residual, guess, args=(Sc,), full_output=True)
-  vec, err = res[0], np.sqrt(np.diag(res[1]))
+  vec, err = res[0], _np.sqrt(_np.diag(res[1]))
 
   return vec[2], err[2]
 #
@@ -131,7 +145,7 @@ def keep_points(x, fname):
   return reduce(operator.or_, segments)
 
 def Lanczos(x, y, xnew):
-  n = np.arange(len(x))
+  n = _np.arange(len(x))
   Ni = interp1d(x, n, kind='linear', fill_value='extrapolate')(xnew)
-  ynew = [np.sum(y*np.sinc(ni-n)) for ni in Ni]
-  return np.array(ynew)
+  ynew = [_np.sum(y*_np.sinc(ni-n)) for ni in Ni]
+  return _np.array(ynew)
