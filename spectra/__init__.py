92b1d9a2a3b2edcb9f0cb9acc908c3b053fa5bbe
mahollands <M.Hollands.1@warwick.ac.uk>
2019-02-27 11:23:09 +0000

    Removed redundant imports from __init__

--- spectra/__init__.py
+++ spectra/__init__.py
@@ -4,8 +4,6 @@ Module for working with astropyhsical spectra
 __author__ = "Mark Hollands"
 __email__ = "M.Hollands.1@warwick.ac.uk"
 
-import numpy as np
-import matplotlib.pyplot as plt
 from .spec_class import * 
 from .spec_io import *
 from .misc import air_to_vac, vac_to_air, voigt, jangstrom
