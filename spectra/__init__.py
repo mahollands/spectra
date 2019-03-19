e5bdeb3d02eba6695491991289021459bc604b94
mahollands <M.Hollands.1@warwick.ac.uk>
2019-03-19 12:01:17 +0000

    updated __init__.py

--- spectra/__init__.py
+++ spectra/__init__.py
@@ -7,4 +7,4 @@ __email__ = "M.Hollands.1@warwick.ac.uk"
 from .spec_class import Spectrum 
 from .spec_io import *
 from .spec_functions import *
-from .misc import air_to_vac, vac_to_air, voigt, jangstrom
+from .misc import air_to_vac, vac_to_air, voigt, jangstrom, logarange
