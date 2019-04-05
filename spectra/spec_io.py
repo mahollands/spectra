37de9dedd148fd72b15c896f4649fe86122aae55
mahollands <M.Hollands.1@warwick.ac.uk>
2019-04-05 11:42:27 +0100

    model_from_dk now reads dk headers into .head

--- spectra/spec_io.py
+++ spectra/spec_io.py
@@ -18,6 +18,15 @@ __all__ = [
   "spec_list_from_molly",
 ]
 
+#element dict for dk headers
+el_dict = {
+   1:'H' ,  2:'He',  3:'Li',  6:'C' ,  7:'N' ,  8:'O' ,  9:'F' ,
+  10:'Ne', 11:'Na', 12:'Mg', 13:'Al', 14:'Si', 15:'P' , 16:'S' ,
+  17:'Cl', 18:'Ar', 19:'K' , 20:'Ca', 21:'Sc', 22:'Ti', 23:'V' ,
+  24:'Cr', 25:'Mn', 26:'Fe', 27:'Co', 28:'Ni', 29:'Cu', 30:'Zn',
+  31:'Ga', 32:'Ge', 38:'Sr', 56:'Ba',
+}
+
 def spec_from_txt(fname, wave='air', x_unit='AA', y_unit='erg/(s cm2 AA)', **kwargs):
   """
   Loads a text file with the first 3 columns as wavelengths, fluxes, errors.
@@ -40,14 +49,29 @@ def model_from_dk(fname, x_unit='AA', y_unit='erg/(s cm2 AA)', **kwargs):
   """
   Similar to model_from_txt, but will autoskip past the DK header. Units are converted 
   """
-  #Measure header first
+  #extract header first
+  hdr = {}
   with open(fname, 'r') as Fdk:
     for skip, line in enumerate(Fdk, 1):
-      if line.startswith("END"):
+      if line.startswith("TEFF"):
+        Teff = float(line.split()[2])
+        hdr['Teff'] = Teff
+      elif line.startswith("LOG_G"):
+        logg = float(line.split()[2])
+        hdr['logg'] = logg
+      elif line.startswith("COMMENT   el"):
+        *_, Z, logZ = line.split() 
+        Z = int(Z)
+        logZ = float(logZ)
+        hdr[el_dict[Z]] = logZ
+      elif line.startswith("END"):
         break
+      else:
+        continue
   M = model_from_txt(fname, 'vac', 'AA', 'erg/(s cm3)', skiprows=skip)
   M.x_unit_to(x_unit)
   M.y_unit_to(y_unit)
+  M.head.update(hdr)
   return M
 
 def spec_from_npy(fname, wave='air', x_unit='AA', y_unit='erg/(s cm2 AA)'):
