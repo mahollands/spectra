640805399e3a3350ac710e5fb05d35f86d73f3d7
mahollands <M.Hollands.1@warwick.ac.uk>
2020-01-22 13:50:49 +0000

    Added JPLUS filters

--- spectra/synphot.py
+++ spectra/synphot.py
@@ -5,25 +5,29 @@ import os.path
 __all__ = [
   "load_transmission_curve",
   "mag_calc_AB",
+  "filter_names",
 ]
 
 filters_dir = "{}/filt_profiles".format(os.path.dirname(__file__))
 
 GaiaDict = {'G':'G', 'Bp':'Gbp', 'Rp':'Grp'}
+JPLUS = "gSDSS iSDSS J0378 J0395 J0410 J0430 J0515 J0660 J0861 rSDSS uJAVA zSDSS".split()
 filter_paths = {
-  **{f"2m{b}"   : f"2MASS_2MASS.{b}.dat" for b in "JHK"}, #2Mass
-  **{f"Denis{b}": f"DENIS_DENIS.{b}.dat" for b in "I"}, #DENIS
-  **{f"Gaia{b}" : f"GAIA_GAIA2r.{GaiaDict[b]}.dat" for b in ("G","Bp","Rp")}, #Gaia
-  **{f"Galex{b}": f"GALEX_GALEX.{b}.dat" for b in ("NUV", "FUV")}, #GALEX
-  **{b          : f"Generic_Johnson.{b}.dat" for b in "UBVRI"}, #Generic Johnson
-  **{f"ps{b}"   : f"PAN-STARRS_PS1.{b}.dat" for b in "grizy"}, #PanStarrs
-  **{b          : f"SLOAN_SDSS.{b}.dat" for b in "ugriz"}, #SDSS
-  **{f"sm{b}"   : f"SkyMapper_SkyMapper.{b}.dat" for b in "uvgriz"}, #SkyMapper
-  **{f"S{b}"    : f"Spitzer_IRAC.I{b}.dat" for b in "12"}, #Spitzer
-  **{f"sw{b}"   : f"Swift_UVOT.{b}.dat" for b in ("U","UVW1","UVW2","UVM2")}, #Swift
-  **{f"UK{b}"   : f"UKIRT_UKIDSS.{b}.dat" for b in "ZYJHK"}, #UKIRT
-  **{f"W{b}"    : f"WISE_WISE.W{b}.dat" for b in "12"}, #Wise
+  **{f"2m{b}"    : f"2MASS_2MASS.{b}.dat" for b in "JHK"}, #2Mass
+  **{f"Denis{b}" : f"DENIS_DENIS.{b}.dat" for b in "I"}, #DENIS
+  **{f"Gaia{b}"  : f"GAIA_GAIA2r.{GaiaDict[b]}.dat" for b in ("G","Bp","Rp")}, #Gaia
+  **{f"Galex{b}" : f"GALEX_GALEX.{b}.dat" for b in ("NUV", "FUV")}, #GALEX
+  **{b           : f"Generic_Johnson.{b}.dat" for b in "UBVRI"}, #Generic Johnson
+  **{f"JPLUS-{b}": f"JPLUS/OAJ_JPLUS.{b}.dat" for b in JPLUS}, #JPLUS
+  **{f"ps{b}"    : f"PAN-STARRS_PS1.{b}.dat" for b in "grizy"}, #PanStarrs
+  **{b           : f"SLOAN_SDSS.{b}.dat" for b in "ugriz"}, #SDSS
+  **{f"sm{b}"    : f"SkyMapper_SkyMapper.{b}.dat" for b in "uvgriz"}, #SkyMapper
+  **{f"S{b}"     : f"Spitzer_IRAC.I{b}.dat" for b in "12"}, #Spitzer
+  **{f"sw{b}"    : f"Swift_UVOT.{b}.dat" for b in ("U","UVW1","UVW2","UVM2")}, #Swift
+  **{f"UK{b}"    : f"UKIRT_UKIDSS.{b}.dat" for b in "ZYJHK"}, #UKIRT
+  **{f"W{b}"     : f"WISE_WISE.W{b}.dat" for b in "12"}, #Wise
 }
+filter_names = list(filter_paths)
 
 def load_transmission_curve(filt):
   """
@@ -73,6 +77,33 @@ def mag_calc_AB(S, filt, NMONTE=1000, Ifun=Itrapz):
   Swift:     ['sw(U,UVW1,UVW2,UVM1)']
 
   WISE:      ['W1','W2']
+  """
+
+  #load filter
+  R = load_transmission_curve(filt)
+  R.wave = S.wave
+
+  #Convert Spectra/filter-curve to Hz/Jy for integrals
+  R.x_unit_to("Hz")
+  S.x_unit_to("Hz")
+  S.y_unit_to("Jy")
+
+  #clip data to filter range and interpolate filter to data axis
+  S = S.clip(np.min(R.x), np.max(R.x))
+  R = R.interp(S)
+
+  #Calculate AB magnitudes, potentially including flux errors
+  if NMONTE == 0:
+    return m_AB_int(S.x, S.y, R.y, Ifun)
+  else:
+    y_mc = lambda S: np.random.normal(S.y, S.e)
+    m = np.array([m_AB_int(S.x, y_mc(S), R.y, Ifun) for i in range(NMONTE)])
+    return np.mean(m), np.std(m)
+#
+
+def lambda_mean(filt, Ifun=Itrapz):
+  """
+  Given one of the filter-names used for 
   """
 
   #load filter
