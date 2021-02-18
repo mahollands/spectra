f163fbce8b4af0c610ed135b9b5c7f37a991d6db
mahollands <M.Hollands.1@warwick.ac.uk>
2021-02-18 10:41:19 +0000

    Added ability to get mu and flux arrays from limb darkening calculations

--- spec_functions.py
+++ spec_functions.py
@@ -1,5 +1,5 @@
 """
-Contains functions for generating spectra or operating on spectra
+Contains functions for generating spectra or operating on spectra.
 """
 import numpy as np
 import astropy.units as u
@@ -162,3 +162,31 @@ def sky_line_fwhm(S, x0, dx=5., return_model=False):
 
     return (res, sl_model(vec, Sc)) if return_model else res
 #
+
+# See Claret et al., A&A 634, 93 (2020)
+limb_functions = {
+    'linear': (1, lambda P, mu: 1-P[0]*(1-mu)),
+    'quad': (2, lambda P, mu: 1-sum(a*(1-mu)**i for i, a in enumerate(P, 1))),
+    'sqroot': (2, lambda P, mu: 1-P[0]*(1-mu)-P[1]*(1-np.sqrt(mu))),
+    'log': (2, lambda P, mu: 1-P[0]*(1-mu)-P[1]*mu*np.log(mu)),
+    'power': (2, lambda P, mu: 1-P[0]*(1-mu**P[1])),
+    'claret': (4, lambda P, mu: 1-sum(a_k*(1-mu**(k/2)) for k, a_k in enumerate(P, 1))),
+}
+
+def calc_limb_darkening_coeffs(MM, band, limb_model='claret', return_fluxes=False):
+    """
+    Calculate limb darkening coefficients for DK models for a given pandpass.
+    MM is a list of models containing angular dependent spectra as a function
+    of mu. Normalised fluxes and mu points can be returned if 'return_fluxes'
+    is true.
+    """
+    n_param, f = limb_functions[limb_model]
+    guess = np.zeros(n_param)
+
+    mu = np.array([M.head['mu'] for M in MM])
+    fluxes = np.array([M.flux_calc_AB(band) for M in MM])
+    fluxes /= np.max(fluxes)
+
+    vec, *_ = leastsq(lambda P : fluxes - f(P, mu), guess)
+
+    return (vec, mu, fluxes) if return_fluxes else vec
