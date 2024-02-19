e812a6ce5c2a7fd69ac404ebbbb9fe53dcc43324
Mark Hollands <m.hollands@sheffield.ac.uk>
2024-02-19 15:05:15 +0000

    spec_io imports no longer required

--- __init__.py
+++ __init__.py
@@ -5,7 +5,6 @@ __author__ = "Mark Hollands"
 __email__ = "M.Hollands.1@warwick.ac.uk"
 
 from .spec_class import Spectrum
-from .spec_io import *
 from .spec_functions import *
 from .misc import air_to_vac, vac_to_air, voigt, jangstrom, logarange
 from .limb_darkening import *
