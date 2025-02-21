05ceebd3f05e6f883e7c21def73043f329c24da4
Mark Hollands <phsvjw@vala.astro.warwick.ac.uk>
2025-02-21 10:02:43 +0000

    Wave now included in __init__ for star imports

--- __init__.py
+++ __init__.py
@@ -6,5 +6,5 @@ __email__ = "M.Hollands.1@warwick.ac.uk"
 
 from .spec_class import Spectrum
 from .spec_functions import *
-from .misc import air_to_vac, vac_to_air, voigt, jangstrom, logarange
+from .misc import Wave, air_to_vac, vac_to_air, voigt, jangstrom, logarange
 from .limb_darkening import *
